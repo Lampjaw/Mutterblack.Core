0adf0dfe4f435fae13418c26ec846a42eacfbcc8
Matthew Tremper <Matthew.Tremper@ipreo.com>
2019-07-05 17:30:07 -0400

    Added UwuTranslator

---
+++ src/Mutterblack.Core/Commands/CommandGroups/UwuTranslatorCommands.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mutterblack.Core.Commands.CommandGroups
+{
+    [CommandGroup("uwutranslator")]
+    public class UwuTranslatorCommands : CommandGroup
+    {
+        private readonly char[] _replaceableChars = {'l', 'r' };
+
+        [CommandGroupAction("translate")]
+        public Task<CommandResult> TranslateText(string text)
+        {
+            var chars = text.ToList();
+            for (var i = 0; i < chars.Count; i++)
+            {
+                if (_replaceableChars.Any(keyChar => char.ToLowerInvariant(chars[i]) == char.ToLowerInvariant(keyChar)))
+                {
+                    chars[i] = char.IsUpper(chars[i]) ? char.ToUpperInvariant('w') : char.ToLowerInvariant('w');
+                }
+            }
+
+            return Task.FromResult(new CommandResult(new string(chars.ToArray())));
+        }
+    }
+}
